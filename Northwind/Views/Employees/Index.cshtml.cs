12c810bb9f39ef64086ab1828d87111687b06198
Juan Vargas <jvargasc@gmail.com>
2019-06-27 19:53:50 -0400

    Commit with the Minimum Viable Deliverable ready to be deployed to Azure.

---
+++ Northwind/Views/Employees/Index.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Northwind.Views.Employees
+{
+	public class IndexModel : PageModel
+    {
+		public IEnumerable<Models.Employees> Employees { get; set; }
+
+		public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ NorthwindDb/01.- Create Login.sql
@@ -0,0 +1,4 @@
+﻿-- In the master Db execute:
+CREATE LOGIN northwinddbuser WITH PASSWORD = '0x4170AC2A2782A1516FE9E13D7322AE482C1BD594'
+​
+ALTER LOGIN [northwinddbuser] ENABLE​
